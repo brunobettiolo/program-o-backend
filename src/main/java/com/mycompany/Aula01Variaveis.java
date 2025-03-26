06d624f401a8dd392c68d678e76cbddb4023f471
bruno bettiolo de oliveira <bruno.bettiolo.oliveira@escola.pr.gov.br>
2025-03-26 13:52:21 +0000

    Aula01Variaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,12 @@
+package com.mycompany;
+
+public class Aula01Variavel { 
+    public static void main(String[] args) {
+        // Variavel logica
+
+        // Tipo primitivo
+        boolean valorlogicoPrimitivo = true;
+
+        // Tipo abstrato
+        boolean valorlogicoAbstrato = false
+}
