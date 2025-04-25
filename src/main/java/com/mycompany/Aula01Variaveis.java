9d7453bf88868bcdea3529a3e66d6a82c2b51353
bruno bettiolo de oliveira <bruno.bettiolo.oliveira@escola.pr.gov.br>
2025-04-25 13:57:21 +0000

    Aula05repeticao

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -1,33 +1,41 @@
 package com.mycompany;
 
 public class Aula01Variaveis {
-
     public static void main(String[] args) {
-        valorogicoAbstrato    // variavel lógica
+        // Varíavel lógica
 
-        // tipo primitivo
+        // Tipo primitivo
         boolean valorLogicoPrimitivo = true;
-
-        //Exibe o Valor
+        // Exibe o valor
         System.out.println(valorLogicoPrimitivo);
 
-        // Topo abstrato
+        // Tipo abstrato
         Boolean valorLogicoAbstrato = false;
+        // Exibe com texto
+        System.out.println("O valor da varíavel valorLogicoAbstrato é: " + valorLogicoAbstrato);
 
-        //Exibe o texto
-        System.out.println("O valor da variavel alorogicoAbstrato é : " + valorLogicoAbstrato);
-
-        // Variaveis inteiras
+        // Variáveis inteiras
 
         // Tipo primitivo
         int valorInteiro = 10; // 32bits
         long valorInteiroLongo = 10L; // 64bits
 
-        // Tipo abstrato 
+        // Tipo abstrato
         Integer valorInteiroAbs = 20;
         Long valorLongoAbs = 20L;
 
-        System.out.println("O valorInteiro é: " + valorInteiro + ", e o valor de valorInteiroLongo é: " + valorInteiroLongo);
+        System.out.println("O valor de valorInteiro é: " + valorInteiro + ", e o valor de valorInteiroLongo é: " + valorInteiroLongo);
 
-    }
+        // Variáveis Decimais
+
+        // Tipo primitivo
+        float numeroFloat = 10.5f; // 32bits
+        double numeroDouble = 10.5; // 64 bits
+
+        //Tipo abstrato
+        Float numeroFloatAbs = 10.5f;
+        Double numeroDoubleAbs = 10.5;
+
+        System.out.println("O valor de numeroFloat é: " + numeroFloat + ", e o valor de numeroDouble é: " + numeroDouble);
+    }    
 }
---
+++ src/main/java/com/mycompany/Aula05Repeticao.java
@@ -0,0 +1,26 @@
+package com.mycompany;
+
+public class Aula05Repeticao {
+
+    public static void main(String[] args) {
+        /**
+         * For (Para)
+         *
+         * Para cada elemento repita
+         * 
+         * 1º variável de iteração
+         * 2º condição de parada
+         * 3º incremento ou decremento
+         */
+        for(int i = 0;i <= 10; i=i +2) {
+            System.out.println(i);
+
+            // Decremento
+            System.out.println("Contagem regressiva...");
+            for (int i = 10; i >= 0; i--) {
+                Thread.sleep(1000L);
+                System.out.println(i + "...");
+        }
+        System.out.println("BOMM!!");
+    }
+
