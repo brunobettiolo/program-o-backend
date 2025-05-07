fa10e0b693c8c199b2b8aec2c1e6c89ce93b972d
bruno bettiolo de oliveira <bruno.bettiolo.oliveira@escola.pr.gov.br>
2025-05-07 13:50:19 +0000

    aula05sobrerepeticões

--- src/main/java/com/mycompany/Aula05Repeticao.java
+++ src/main/java/com/mycompany/Aula05Repeticao.java
@@ -1,8 +1,10 @@
 package com.mycompany;
 
+import java.util.Scanner;
+
 public class Aula05Repeticao {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /**
          * For (Para)
          *
@@ -12,15 +14,27 @@ public class Aula05Repeticao {
          * 2º condição de parada
          * 3º incremento ou decremento
          */
-        for(int i = 0;i <= 10; i=i +2) {
+        for (int i = 0; i <= 10; i = i + 2) {
             System.out.println(i);
-
-            // Decremento
-            System.out.println("Contagem regressiva...");
-            for (int i = 10; i >= 0; i--) {
-                Thread.sleep(1000L);
-                System.out.println(i + "...");
+        }
+        
+        // Decremento
+        System.out.println("Contagem regressiva...");
+        for (int i = 10; i >= 0; i--) {
+            Thread.sleep(1000L);
+            System.out.println(i + "...");
         }
         System.out.println("BOMM!!");
-    }
 
+        /**
+         * Classe Scanner
+         *
+        * É utilizada para ler valores do usuário inseridos pelo prompt.
+        */
+        Scanner scan = new Scanner(System.in); // Cria o scanner
+        System.out.println("Digite um número: ");
+
+        int numero = scan.nextInt(); // Lê o valor digitado e capturado pelo scanner
+        System.out.println("Você digitou " + numero + "!!");
+    }
+}
