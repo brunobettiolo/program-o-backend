91c0b2adaf4362f7ae60638a8316078133e0906c
bruno bettiolo de oliveira <bruno.bettiolo.oliveira@escola.pr.gov.br>
2025-05-09 13:21:10 +0000

    aula05sobrerepetição

--- src/main/java/com/mycompany/Aula05Repeticao.java
+++ src/main/java/com/mycompany/Aula05Repeticao.java
@@ -17,7 +17,7 @@ public class Aula05Repeticao {
         for (int i = 0; i <= 10; i = i + 2) {
             System.out.println(i);
         }
-        
+
         // Decremento
         System.out.println("Contagem regressiva...");
         for (int i = 10; i >= 0; i--) {
@@ -29,12 +29,40 @@ public class Aula05Repeticao {
         /**
          * Classe Scanner
          *
-        * É utilizada para ler valores do usuário inseridos pelo prompt.
-        */
-        Scanner scan = new Scanner(System.in); // Cria o scanner
-        System.out.println("Digite um número: ");
+         * É utilizada para ler valores do usuário inseridos pelo prompt.
+         */
+        // Scanner scan = new Scanner(System.in); // Cria o scanner
+        // System.out.println("Digite um número: ");
 
-        int numero = scan.nextInt(); // Lê o valor digitado e capturado pelo scanner
-        System.out.println("Você digitou " + numero + "!!");
+        // int numero = scan.nextInt(); // Lê o valor digitado e capturado pelo scanner
+        // System.out.println("Você digitou " + numero + "!!");
+
+        /**
+         * While (enquanto)
+         *
+         * Enquanto a condição for verdadeira o código será executado
+         * diferente da próxima estrutura, essa faz a validação antes da execução
+         */
+        int num1 = 1;
+
+        while (num1 != 0) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite um número: (0 para parar)");
+            num1 = scan.nextInt();
+            System.out.println("Você digitou " + num1 + "!!");
+        }
+        System.out.println("Execução encerrada");
+        /**
+         * Do While (execute, enquanto)
+         *
+         * É executada uma estrutura de código e após é validada a condição
+         */
+        int num2 = 1;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite um número: (0 para Sair)");
+            num2 = scan.nextInt();
+        } while (num2 != 0);
+        System.out.println("Execução encerrada");
     }
 }
